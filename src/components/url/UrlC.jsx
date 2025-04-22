67cc81bcb4890fec53fc7228185ceca15cd4ff01
Facundo <facundodelacruz01@gmail.com>
2025-04-22 16:40:55 -0300

    se arregla el problema de las rutas en el front

--- src/components/url/UrlC.jsx
+++ src/components/url/UrlC.jsx
@@ -37,6 +37,7 @@ export const UrlC = () => {
   }, [index, reverse]);
 
   const API_URL = import.meta.env.VITE_API_URL;
+  console.log("API URL:", API_URL);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
